03aced8a4c3ec21a1549eb815d2a6157300e0d45
Christian Deniel <christian.ambro@hotmail.com>
2023-11-15 21:49:41 -0300

    feat: Tela quase pronta, falta ícone.

--- src/screens/queue-modal/index.tsx
+++ src/screens/queue-modal/index.tsx
@@ -1,7 +1,12 @@
 import React, { type ReactElement } from 'react'
 import { Modal, TouchableOpacity } from 'react-native'
-import { ContainerModal } from './style'
+import { ContainerModal, TOpp,TOpp2, Botoes, Faixa } from './style'
 import Text from '../../core/base-text/base-text'
+import {IconDefinition} from '@fortawesome/fontawesome-svg-core';
+import {faBookDead, faBookOpenReader} from '@fortawesome/free-solid-svg-icons';
+import { LeadingIcon } from '../../core/base-screen/components/screen-header/screen-header.styles';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+
 
 interface Props {
   modalVisible: boolean
@@ -27,16 +32,22 @@ export default function QueueModal ({
   return (
     <Modal visible={modalVisible} animationType="fade" transparent>
       <ContainerModal>
-        <TouchableOpacity
+      <Faixa></Faixa>
+      
+      <Text style={{textAlign:'center',fontSize: 25,color: 'red'}}>LIVRO JÁ EMPRESTADO!</Text>
+      <Text style={{textAlign:'center',width: 250,alignSelf: 'center'}}>Deseja adicionar o usuário a Fila de Espera?</Text>
+        <Botoes>
+        <TOpp
           onPress={() => {
             action()
           }}
-          style={{
-            backgroundColor: 'green',
-            height: 100,
-            width: 100
-          }}></TouchableOpacity>
-        <Text>Você foi adicionado na fila de espera</Text>
+          ><Text style={{color: 'white'}}>CONFIRMAR</Text></TOpp>
+          <TOpp2
+          onPress={() => {
+            action()
+          }}
+          ><Text style={{color: '#733119'}}>CANCELAR</Text></TOpp2>
+          </Botoes>
       </ContainerModal>
     </Modal>
   )
